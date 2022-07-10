ceb9f1ac56c48a5789ac1b840b579fded09591c7
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-10 23:20:32 +0900

    int

--- sample.go
+++ sample.go
@@ -2,46 +2,18 @@
 // ちなみにパッケージの宣言は 1つまでしかできない
 package main
 
-// fmtモジュールをインポート
 import "fmt"
 
-func outer() {
-	var s4 string = "outerやで！"
-	fmt.Println(s4)
-}
+// fmtモジュールをインポート
 
 func main() {
-	var i int = 100
-	fmt.Println(i)
 
-	var s string = "Hello World"
-	fmt.Println(s)
+		var i string = "100"
 
-	var t, f bool = true, false
-	fmt.Println(t, f)
+		var i2 int64 = 200
 
-	var (
-		i2 int = 100
-		a2 string = "Golang"
-	)
+		fmt.Printf("%T\n", i)
 
-	fmt.Println(i2, a2)
 
-	// 初期値がない時は0が返る
-	var i3 int
-	var s3 string
-	fmt.Println(i3, s3)
-
-	i3 = 300
-	s3 = "GOGOGOGO"
-	fmt.Println(i3, s3)
-
-	i4 := 666
-	fmt.Println(i4)
-
-	i4 = 342134
-	fmt.Println(i4)
-
-	outer()
-
+		fmt.Printf("%T\n", int32(i2))
 }
