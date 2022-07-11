ccd354a1b8762ded274fbe307317cd0d1c74820f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-11 09:28:31 +0900

    string

--- sample.go
+++ sample.go
@@ -7,25 +7,14 @@ import "fmt"
 // fmtモジュールをインポート
 
 func main() {
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	var s string = "Hello golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
 
-	fl := 3.2
-	fmt.Println(fl + fl64)
+	fmt.Println(`test
+		test
+	test
+	`)
 
-	fmt.Printf("%T, %T\n", fl64, fl)
-
-
-	var fl32  float32 =1.2
-	fmt.Printf("%T\n", float64(fl32))
-
-	zero := 0.0
-	pinf := 1.0 / zero
-	fmt.Println(pinf)
-
-	ninf := -1.0 / zero
-	fmt.Println(ninf)
-
-	nan := zero / zero
-	fmt.Println(nan)
+	fmt.Println(string(s[0]))
 }
