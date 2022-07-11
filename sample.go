cf8c26338c09048605b497217f9e1628292bddfe
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-11 10:11:38 +0900

    interface

--- sample.go
+++ sample.go
@@ -7,18 +7,7 @@ import "fmt"
 // fmtモジュールをインポート
 
 func main() {
-	var arr1 [3]int
-	fmt.Println(arr1)
-	fmt.Printf("%T\n",arr1)
-
-	var arr2 [3] string = [3]string{"A","B"}
-	fmt.Println(arr2)
-
-	arr3 := [3]int{1,2,3}
-	fmt.Println(arr3)
-
-	arr4 := [...]string{ "C", "D" }
-	fmt.Println(arr4)
-
-	fmt.Println(arr3[0])
+	var x interface {}
+	x = 1
+	fmt.Println(x)
 }
