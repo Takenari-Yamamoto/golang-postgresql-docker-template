465caabeeb0f0cdb65dfa2ccb9d0de7c4f5c7ae1
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-12 08:37:53 +0900

    convert

--- sample.go
+++ sample.go
@@ -2,12 +2,30 @@
 // ちなみにパッケージの宣言は 1つまでしかできない
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // fmtモジュールをインポート
 
 func main() {
-	var x interface {}
-	x = 1
-	fmt.Println(x)
+	// var i int = 1
+	// fl64 := float64(i)
+	// fmt.Println(fl64)
+	// fmt.Printf("i = %T\n", fl64)
+
+	// i2 := int(fl64)
+	// fmt.Printf("i2 = %T\n", i2)
+
+	// fl := 10.5
+	// i3 := int(fl)
+	// fmt.Printf("i3 = %T\n", i3)
+	// fmt.Println(i3)
+
+	var s string = "100"
+	i, e := strconv.Atoi(s)
+	fmt.Println(i, e)
+	fmt.Printf("i = %T\n", i)
+	fmt.Printf("i = %T\n", e)
 }
