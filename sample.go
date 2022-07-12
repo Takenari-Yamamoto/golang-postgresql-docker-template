52ab8cc1db1575f4a7f446ff9695d08a330034a1
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-12 09:05:45 +0900

    定数

--- sample.go
+++ sample.go
@@ -2,30 +2,42 @@
 // ちなみにパッケージの宣言は 1つまでしかできない
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 // fmtモジュールをインポート
 
+// 頭文字を大文字にすることで他のパッケージからも呼び出せる
+const Pi = 62
+
+const (
+	URL = "http://xxx.com"
+	SiteName = "test"
+)
+
+const (
+	A = 1
+	B
+	C
+	D = "A"
+	E
+	F
+)
+
+const (
+	c0 = iota
+	c1
+	c2
+)
+
+const Big = 913413413413413453 + 1
+
 func main() {
-	// var i int = 1
-	// fl64 := float64(i)
-	// fmt.Println(fl64)
-	// fmt.Printf("i = %T\n", fl64)
+	fmt.Println(Pi)
 
-	// i2 := int(fl64)
-	// fmt.Printf("i2 = %T\n", i2)
+	fmt.Println(URL, SiteName)
+	fmt.Println(A, B, C, D, E, F)
 
-	// fl := 10.5
-	// i3 := int(fl)
-	// fmt.Printf("i3 = %T\n", i3)
-	// fmt.Println(i3)
+	fmt.Println(Big - 1)
 
-	var s string = "100"
-	i, e := strconv.Atoi(s)
-	fmt.Println(i, e)
-	fmt.Printf("i = %T\n", i)
-	fmt.Printf("i = %T\n", e)
+	fmt.Println(c0, c1, c2)
 }
