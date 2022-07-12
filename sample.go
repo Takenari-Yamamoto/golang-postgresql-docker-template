4b42ce0556e9a839d3ddee61434835589545541c
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-12 09:37:50 +0900

    無名関数

--- sample.go
+++ sample.go
@@ -4,37 +4,15 @@ package main
 
 import "fmt"
 
-// fmtモジュールをインポート
-
-func Plus (x , y int ) int {
-	return x + y
-}
-
-func Div (x, y int) (int, int) {
-	q := x / y
-	r := x % y
-	return q, r
-}
-
-func Double(price int)(result int){
-	result = price * 2
-	return
-}
-
-func Noreturn() {
-	fmt.Println("No Return")
-	// return
-}
-
 func main() {
-	i := Plus(1, 2)
+	f := func(x, y int) int {
+		return x + y
+	}
+	i := f(1, 2)
 	fmt.Println(i)
 
-	i2, i3 := Div(9, 3)
-	fmt.Println(i2, i3)
-
-	i4 := Double(1000)
-	fmt.Println(i4)
-
-	Noreturn()
+	i2 := func(x, y int) int {
+		return x + y
+	}(1, 2)
+	fmt.Println(i2)
 }
