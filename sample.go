2516f46dcb3e84a68403720ef9091883e4cf4b30
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-15 09:23:48 +0900

    slice

--- sample.go
+++ sample.go
@@ -4,13 +4,23 @@ package main
 
 import "fmt"
 
-func init() {
-	fmt.Println("init")
-}
-func init() {
-	fmt.Println("init2")
-}
-
 func main() {
-	fmt.Println("Main")
+	// var sl []int
+	// fmt.Println(sl)
+
+	// var sl2 []int = []int{100, 200}
+	// fmt.Println(sl2)
+	// sl2[0] = 1000
+	// fmt.Println(sl2)
+
+	// sl3 := []string{"A", "B"}
+	// fmt.Println(sl3)
+
+	// sl4 := make([]int, 5)
+	// fmt.Println(sl4)
+
+	sl5 := []int {1,2,3,4,5}
+	fmt.Println(sl5[0])
+	fmt.Println(sl5[2:5])
+
 }
