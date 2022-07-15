59f594054f1dede14407599e37a2da854b96235e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-15 10:10:02 +0900

    スライスのコピー

--- sample.go
+++ sample.go
@@ -4,15 +4,19 @@ package main
 
 import "fmt"
 func main() {
-	// sl = append(sl, 300)
-	// // fmt.Println(sl)
+//  sl := []int{100, 200}
+//  sl2 := sl
+//  sl2[0] = 1000
+//  fmt.Println(sl2)
+//  fmt.Println(sl)
 
-	// sl = append(sl, 400, 500, 600)
-	sl := []int{100, 200, 300, 400, 500}
-	fmt.Println(cap(sl))
-	fmt.Println(len(sl))
+//  var i int = 10
+//  i2 := i
+//  i2 = 100
+//  fmt.Println(i, i2)
 
+	sl := []int{1,2,3,4,5}
 	sl2 := make([]int, 5, 10)
-	fmt.Println(cap(sl2))
-	fmt.Println(len(sl2))
+	n := copy(sl2, sl)
+	fmt.Println(n, sl, sl2)
 }
