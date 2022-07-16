6c7f48f3a99abfe0cdb2ea38e5d5d5febf558a3b
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 13:51:50 +0900

    chanel

--- sample.go
+++ sample.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func receiver(name string, ch chan int) {
@@ -18,26 +17,56 @@ func receiver(name string, ch chan int) {
 
 func main() {
 	ch1 := make(chan int, 2)
+	ch2 := make(chan string, 2)
 
+	// ch2 <- "A"
 	// ch1 <- 1
-	// close(ch1)
+	// ch2 <- "B"
+	// ch1 <- 2
 
-	// i, ok := <-ch1
-	// fmt.Println(i, ok)
+	// v1 := <-ch2
+	// v2 := <-ch2
+	// fmt.Println(v1)
+	// fmt.Println(v2)
 
-	// i2, ok := <-ch1
-	// fmt.Println(i2, ok)
-
-	go receiver("1.goroutine", ch1)
-	go receiver("2.goroutine", ch1)
-	go receiver("3.goroutine", ch1)
-
-	i := 0
-	for i < 100 {
-		ch1 <- i
-		i++
+	select {
+		case v1 := <-ch1:
+			fmt.Println(v1 + 1000)
+		case v2 := <-ch2:
+			fmt.Println(v2 + "!?")
+		default:
+			fmt.Println("どちらでもない")
 	}
-	close(ch1)
-	time.Sleep(3 * time.Second)
+
+	ch3 := make(chan int)
+	ch4 := make(chan int)
+	ch5 := make(chan int)
+
+	go func() {
+		for {
+			i := <-ch3
+			ch4 <- i * 2
+		}
+	}()
+
+	go func() {
+		for {
+			i2 := <-ch4
+			ch5 <- i2 - 1
+		}
+	}()
+
+	n := 0
+	for {
+		select {
+			case ch3 <- n:
+				n++
+			case i3 := <- ch5:
+				fmt.Println("receive", i3)
+			if n > 100 {
+				break
+			}
+		}
+	}
 
 }
