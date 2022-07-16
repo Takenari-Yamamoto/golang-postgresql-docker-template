86d325669309a5d31ee1d700b50db602e066fb20
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 19:24:35 +0900

    struct

--- sample.go
+++ sample.go
@@ -2,31 +2,55 @@ package main
 
 import "fmt"
 
-func Double(i int) {
-	i = i * 2
+type User struct {
+	Name string
+	Age int
 }
 
-func DoubleV2(i *int) {
-	*i = *i * 2
+func UpdateUser (user User) {
+	user.Name = "A"
+	user.Age = 1000
+}
+
+func UpdateUser2 (user *User) {
+	user.Name = "A"
+	user.Age = 1000
 }
 
 func main() {
-	var n int = 100
-	fmt.Println(n)
-	fmt.Println(&n)
+	var user1 User
 
-	Double(n)
+	user1.Name = "Takenari"
+	user1.Age = 24
+	fmt.Println(user1)
 
-	fmt.Println(n)
+	user2 := User{}
+	user2.Name = "Bob"
+	fmt.Println(user2)
 
-	var p *int = &n
-	fmt.Println(p)
-	fmt.Println(*p)
+	user3 := User{Name: "user3", Age: 34}
+	fmt.Println(user3)
 
-	*p = 6000
-	fmt.Println(n)
+	user4 := User{"Sake", 20}
+	fmt.Println(user4)
 
-	DoubleV2(&n)
-	fmt.Println(n)
+	user5 := User{ "user5", 300}
+	fmt.Println(user5)
+
+	user6 := User{Name: "user6"}
+	fmt.Println(user6)
+
+	// ポインタ型で変数定義する方法①
+	user7 := new(User)
+	fmt.Println(user7)
+
+	// ポインタ型で変数定義する方法②
+	user8 := &User{}
+	fmt.Println(user8)
+
+	UpdateUser(user1)
+	UpdateUser2(user8)
+	fmt.Println(user1)
+	fmt.Println(user8)
 
 }
