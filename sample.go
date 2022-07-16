970e515324c6a5e0244a26f6831f089f27a8811c
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 14:13:22 +0900

    pointer

--- sample.go
+++ sample.go
@@ -1,72 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func receiver(name string, ch chan int) {
-	for {
-		i, ok := <-ch
-		if !ok {
-			break
-		}
-		fmt.Println(name, i)
-	}
-	fmt.Println(name + "END")
+func Double(i int) {
+	i = i * 2
+}
+
+func DoubleV2(i *int) {
+	*i = *i * 2
 }
 
 func main() {
-	ch1 := make(chan int, 2)
-	ch2 := make(chan string, 2)
+	var n int = 100
+	fmt.Println(n)
+	fmt.Println(&n)
 
-	// ch2 <- "A"
-	// ch1 <- 1
-	// ch2 <- "B"
-	// ch1 <- 2
+	Double(n)
 
-	// v1 := <-ch2
-	// v2 := <-ch2
-	// fmt.Println(v1)
-	// fmt.Println(v2)
+	fmt.Println(n)
 
-	select {
-		case v1 := <-ch1:
-			fmt.Println(v1 + 1000)
-		case v2 := <-ch2:
-			fmt.Println(v2 + "!?")
-		default:
-			fmt.Println("どちらでもない")
-	}
+	var p *int = &n
+	fmt.Println(p)
+	fmt.Println(*p)
 
-	ch3 := make(chan int)
-	ch4 := make(chan int)
-	ch5 := make(chan int)
+	*p = 6000
+	fmt.Println(n)
 
-	go func() {
-		for {
-			i := <-ch3
-			ch4 <- i * 2
-		}
-	}()
-
-	go func() {
-		for {
-			i2 := <-ch4
-			ch5 <- i2 - 1
-		}
-	}()
-
-	n := 0
-	for {
-		select {
-			case ch3 <- n:
-				n++
-			case i3 := <- ch5:
-				fmt.Println("receive", i3)
-			if n > 100 {
-				break
-			}
-		}
-	}
+	DoubleV2(&n)
+	fmt.Println(n)
 
 }
