807cf931871da8820b57f02cb02ae8e6b3c1f80d
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 21:06:34 +0900

    slice

--- sample.go
+++ sample.go
@@ -2,21 +2,32 @@ package main
 
 import "fmt"
 
-type T struct {
-	User
-}
-
 type User struct {
 	Name string
 	Age int
 }
 
-func NewUser (name string, age int) *User {
-	return &User{Name: name, Age: age}
-}
+type Users []*User
 
 func main() {
-	user1 := NewUser("user1", 10)
-	fmt.Println(user1)
-	fmt.Println(*user1)
+	user1 := User{Name: "user1", Age: 10}
+	user2 := User{Name: "user2", Age: 20}
+	user3 := User{Name: "user3", Age: 30}
+	user4 := User{Name: "user4", Age: 40}
+
+	users := Users{}
+	users = append(users, &user1)
+	users = append(users, &user2)
+	users = append(users, &user3, &user4)
+
+	for _, u := range users {
+		fmt.Println(*u)
+	}
+
+	users2 := make([]*User, 0)
+	users2 = append(users2, &user1, &user2)
+
+	for _, u := range users2 {
+		fmt.Println(*u)
+	}
 }
