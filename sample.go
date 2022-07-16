127521976480ce8d2f6a13cc5f2f21a1008054fa
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 10:44:51 +0900

    値の取り出し

--- sample.go
+++ sample.go
@@ -4,14 +4,30 @@ package main
 
 import "fmt"
 
-func Sum(s ...int)int {
-	n := 0
-	for _, v := range s {
-		n += v
-	}
-	return n
-}
 
 func main() {
-	fmt.Println(Sum(1, 2, 3))
+	// var m = map[string]int {"A":100}
+	// fmt.Println(m)
+
+	// m2 := map[string]int {"A":100, "B": 200}
+	// fmt.Println(m2)
+
+	// m3 := map[int]string{
+	// 	1: "A",
+	// 	2: "B",
+	// }
+	// fmt.Println(m3)
+
+	// m4 := make(map[int]string)
+	// fmt.Println(m4)
+	// m4[1] = "A"
+	// m4[2] = "B"
+	// // fmt.Println(m4)
+
+	var m5 = map[string]string {"A":"HOGE"}
+	fmt.Println(m5["A"])
+	fmt.Println(m5["C"])
+
+	s, ok := m5["A"]
+	fmt.Println(s, ok)
 }
