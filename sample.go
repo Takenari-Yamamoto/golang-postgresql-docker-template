9f3e87911d99e7adc22375d781acf6b11714cf5a
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 11:45:36 +0900

    chanel

--- sample.go
+++ sample.go
@@ -1,33 +1,46 @@
-// mainパッケージであることを宣言
-// ちなみにパッケージの宣言は 1つまでしかできない
 package main
 
 import "fmt"
 
-
 func main() {
-	// var m = map[string]int {"A":100}
-	// fmt.Println(m)
+	// var ch1 chan int
 
-	// m2 := map[string]int {"A":100, "B": 200}
-	// fmt.Println(m2)
+	// ch1 = make(chan int)
 
-	// m3 := map[int]string{
-	// 	1: "A",
-	// 	2: "B",
-	// }
-	// fmt.Println(m3)
+	// fmt.Println(cap(ch1))
+	// ch2 := make(chan int)
+	// fmt.Println(cap(ch2))
 
-	// m4 := make(map[int]string)
-	// fmt.Println(m4)
-	// m4[1] = "A"
-	// m4[2] = "B"
-	// // fmt.Println(m4)
 
-	var m5 = map[string]string {"A":"HOGE"}
-	fmt.Println(m5["A"])
-	fmt.Println(m5["C"])
+	// データの操作
+	// データの送受信を行うのがチャネル
+	ch3 := make(chan int, 5)
 
-	s, ok := m5["A"]
-	fmt.Println(s, ok)
+	// ① データをチャネルに送信する
+	// 下記の例は ch3 に 1というデータを送信している
+	ch3 <- 1
+	// データの要素数は len() で調べることができる
+	fmt.Println(len(ch3)) // 1
+
+	ch3 <- 2
+	ch3 <- 3
+	fmt.Println("長さ", len(ch3)) // 3
+
+	i := <-ch3
+	fmt.Println("長さ", len(ch3))
+	fmt.Println(i)
+	i2 := <-ch3
+	fmt.Println("長さ", len(ch3))
+	fmt.Println(i2)
+	i3 := <-ch3
+	fmt.Println("長さ", len(ch3))
+	fmt.Println(i3)
+
+	ch3 <- 1
+	ch3 <- 2
+	ch3 <- 3
+	ch3 <- 4
+	ch3 <- 5
+	// ch3 <- 6
+
 }
