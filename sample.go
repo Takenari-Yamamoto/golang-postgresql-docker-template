4777e3471137f4a40a1db239b59c1296291fcbb0
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 23:17:58 +0900

    カスタムエラー

--- sample.go
+++ sample.go
@@ -2,35 +2,20 @@ package main
 
 import "fmt"
 
-type Stringfy interface {
-	ToString() string
+type MyError struct {
+	Message string
+	ErrCode int
 }
 
-type Person struct {
-	Name string
-	Age int
+func (e *MyError) Error() string {
+	return e.Message
 }
 
-func (p *Person) ToString() string {
-	return fmt.Sprintf("Name=%v, Age=%v", p.Name, p.Age)
-}
-
-type Car struct {
-	Number string
-	Model string
-}
-
-func (c *Car) ToString() string {
-	return fmt.Sprintf("Number=%v, Model=%v", c.Number, c.Model)
+func RaiseError () error {
+	return &MyError{Message: "カスタムエラーが発生しました", ErrCode: 1234}
 }
 
 func main() {
-	vs := []Stringfy{
-		&Person{Name: "Taro", Age: 21},
-		&Car{Number: "123-456", Model: "AB-1234"},
-	}
-
-	for _, v := range vs {
-		fmt.Println(v.ToString())
-	}
+	err := RaiseError()
+	fmt.Println(err.Error())
 }
