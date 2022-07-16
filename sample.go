d2b232797dca4854062f1ab93ff7ef7b4e351186
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 20:41:42 +0900

    constructer

--- sample.go
+++ sample.go
@@ -2,39 +2,21 @@ package main
 
 import "fmt"
 
+type T struct {
+	User
+}
+
 type User struct {
 	Name string
 	Age int
 }
 
-// レシーバー宣言
-func (u User) SayName() {
-	fmt.Println(u.Name)
-}
-
-func (u User) SayAge() {
-	fmt.Println(u.Age)
-}
-
-func (u User) SetName(name string) {
-	u.Name = name
-}
-
-func (u *User) SetName2(name string) {
-	u.Name = name
+func NewUser (name string, age int) *User {
+	return &User{Name: name, Age: age}
 }
 
 func main() {
-	user1 := User{Name: "user1"}
-	user1.SayName()
-
-	user1.SetName("A")
-	user1.SayName()
-
-	user1.SetName2("HOGEHOGEHOGE")
-	user1.SayName()
-
-	user2 := &User{Name: "user2"}
-	user2.SetName("BB")
-	user2.SayName()
+	user1 := NewUser("user1", 10)
+	fmt.Println(user1)
+	fmt.Println(*user1)
 }
