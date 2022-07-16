3c9e19f7fa7f0789433436540af7a350e0cf7441
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-16 12:46:47 +0900

    channel

--- sample.go
+++ sample.go
@@ -1,46 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func receiver(c chan int) {
+	for {
+		i := <-c
+		fmt.Println(i)
+	}
+}
 
 func main() {
-	// var ch1 chan int
+	// Go のチャネルはゴルーチン間でのデータの共有のために使われている
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	// fmt.Println(<-ch1)
 
-	// ch1 = make(chan int)
+	go receiver(ch1)
+	go receiver(ch2)
 
-	// fmt.Println(cap(ch1))
-	// ch2 := make(chan int)
-	// fmt.Println(cap(ch2))
-
-
-	// データの操作
-	// データの送受信を行うのがチャネル
-	ch3 := make(chan int, 5)
-
-	// ① データをチャネルに送信する
-	// 下記の例は ch3 に 1というデータを送信している
-	ch3 <- 1
-	// データの要素数は len() で調べることができる
-	fmt.Println(len(ch3)) // 1
-
-	ch3 <- 2
-	ch3 <- 3
-	fmt.Println("長さ", len(ch3)) // 3
-
-	i := <-ch3
-	fmt.Println("長さ", len(ch3))
-	fmt.Println(i)
-	i2 := <-ch3
-	fmt.Println("長さ", len(ch3))
-	fmt.Println(i2)
-	i3 := <-ch3
-	fmt.Println("長さ", len(ch3))
-	fmt.Println(i3)
-
-	ch3 <- 1
-	ch3 <- 2
-	ch3 <- 3
-	ch3 <- 4
-	ch3 <- 5
-	// ch3 <- 6
-
+	i := 0
+	for i < 100 {
+		ch1 <- i
+		ch2 <- i
+		time.Sleep(10 * time.Millisecond)
+		i ++
+	}
 }
