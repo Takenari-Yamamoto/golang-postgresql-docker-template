e27df553a5a91a2d25693fb842e9ae0b1c8c80e8
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-17 16:57:43 +0900

    delete

--- sample.go
+++ sample.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -21,23 +20,11 @@ func main() {
 	defer Db.Close()
 
 	// 複数データの取得
-	cmd := "SELECT * FROM persons"
-	// Query は条件に合うものを全て取得
-	rows, _ := Db.Query(cmd)
-	defer rows.Close()
-	var pp []Person
-	for rows.Next(){
-		var p Person
-		err := rows.Scan(&p.Name, &p.Age)
+	cmd := "DELETE FROM persons WHERE name = ?"
+	_ ,err := Db.Exec(cmd,"hanako")
 
-		if err != nil {
-			log.Println(err)
-		}
-		pp = append(pp, p)
-	}
-
-	for _, p := range pp {
-		fmt.Println(p.Name, p.Age)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 }
