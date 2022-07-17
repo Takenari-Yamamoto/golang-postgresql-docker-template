92fe1f868632927ddcf0b4812bcb91b633461c28
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-17 10:24:39 +0900

    flag

--- sample.go
+++ sample.go
@@ -1,44 +1,27 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	// os.Exit(1)
-	// fmt.Println("Start")
+	var (
+		max int
+		msg string
+		x bool
+	)
 
-	// defer func () {
-	// 	fmt.Println("defer")
-	// }()
-	// os.Exit(0)
 
-	// _, err := os.Open("A.txt")
-	// if err != nil {
-	// 	log.Fatalln(123, err)
-	// }
+	// IntVar 整数オプション
+	// flag.IntVar(&変数名, "オプションの名前", デフォルト値, "オプションの説明文")
+	flag.IntVar(&max, "n", 32, "処理数の最大値")
+	flag.StringVar(&msg, "m", "", "処理メッセージ")
+	flag.BoolVar(&x, "x", false, "拡張オプション")
 
-		// Args
-		// fmt.Println(os.Args[0])
-		// fmt.Println(os.Args[1])
-		// fmt.Println(os.Args[2])
-		// fmt.Println(os.Args[3])
+	flag.Parse()
 
-		// // os.Argsの要素数を表示
-		// fmt.Printf("length=%d\n", len(os.Args))
-		// for i, v := range os.Args {
-		// 	fmt.Println(i, v)
-		// }
-
-		// f, err := os.Open("text.txt")
-		// if err != nil {
-		// 	log.Fatalln(err)
-		// }
-
-		// defer f.Close()
-
-		f, _ := os.Create("foo.txt")
-		f.Write([]byte("Hello\n"))
-		f.WriteAt([]byte("Golang"), 6)
-		f.Seek(0, os.SEEK_END)
-		f.WriteString("Yeah")
-
+	fmt.Println(max)
+	fmt.Println(msg)
+	fmt.Println(x)
 }
