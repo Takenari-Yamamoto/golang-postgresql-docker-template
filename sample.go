eebe097b9bf905d916f05aa0bdc035bafcac5259
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-17 10:43:02 +0900

    bufio

--- sample.go
+++ sample.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var (
-		max int
-		msg string
-		x bool
-	)
 
+	scanner := bufio.NewScanner(os.Stdin)
 
-	// IntVar 整数オプション
-	// flag.IntVar(&変数名, "オプションの名前", デフォルト値, "オプションの説明文")
-	flag.IntVar(&max, "n", 32, "処理数の最大値")
-	flag.StringVar(&msg, "m", "", "処理メッセージ")
-	flag.BoolVar(&x, "x", false, "拡張オプション")
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 
-	flag.Parse()
-
-	fmt.Println(max)
-	fmt.Println(msg)
-	fmt.Println(x)
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "読み込みエラー: ", err)
+	}
 }
