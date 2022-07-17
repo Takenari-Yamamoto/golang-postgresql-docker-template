3ad65406e8bba759b5c390a9e40cfac2643daa75
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-18 01:40:39 +0900

    delete

--- todo_app/app/models/users.go
+++ todo_app/app/models/users.go
@@ -62,3 +62,13 @@ func (u *User) UpdateUser() (err error) {
 
 	return err
 }
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- todo_app/main.go
+++ todo_app/main.go
@@ -32,4 +32,8 @@ func main () {
 	u, _ = models.GetUser(1)
 	fmt.Println(u)
 
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
 }
