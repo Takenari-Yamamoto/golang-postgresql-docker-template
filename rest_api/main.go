3d1568981bd1430e7c221c5c7d9ff6bdc01cfa54
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-07-28 09:32:49 +0900

    todo ap

---
+++ rest_api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID string `json:"id"`
+	Item string  `json:"item"`
+	Completed bool `json:"completed"`
+}
+
+var todos = []todo {
+	{ ID: "1", Item: "Clean Room", Completed: false },
+	{ ID: "2", Item: "Read Book", Completed: false },
+	{ ID: "3", Item: "Record Video", Completed: false },
+}
+
+func getTodos (context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main () {
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:9090")
+}
