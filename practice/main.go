4b06cd59689f99bc84043b82e2e461b91998d373
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-01-21 15:37:44 +0900

    codeing

--- practice/main.go
+++ practice/main.go
@@ -7,32 +7,30 @@ type User struct {
 	Age int
 }
 
-type Users []*User
-
-func NewUser(name string, age int) *User {
-	return &User{Name: name, Age: age}
-}
-
 func main() {
-	user1 := User{Name: "User1", Age: 10}
-	user2 := User{Name: "User2", Age: 20}
-	user3 := User{Name: "User3", Age: 30}
-	user4 := User{Name: "User4", Age: 40}
-
-	users := Users{}
-
-	users = append(users, &user1, &user2, &user3, &user4)
-
-	fmt.Println(users)
-
-	for _, u := range users {
-		fmt.Println(u)
+	m := map[int]User{
+		1: {Name:"Takenari", Age: 25},
+		2: {Name:"Taro", Age: 5},
+		3: {Name:"Ti", Age: 25},
+		4: {Name:"nari", Age: 25},
 	}
 
-	users2 := make([]*User, 0)
-	users2 = append(users, &user1, &user2, &user3, &user4)
+	fmt.Println(m)
 
-	for _, u := range users2 {
-		fmt.Println(*u)
+	m2 := map[User]string{
+		{Name:"Takenari", Age: 25}: "Tokyo",
+		{Name:"Ti", Age: 25}: "LA",
 	}
+
+	fmt.Println(m2)
+
+	m3 := make(map[int]User)
+	fmt.Println(m3)
+	m3[1] = User{Name: "user3"}
+	fmt.Println(m3)
+
+	for _, v := range m3 {
+		fmt.Println(v)
+	}
+
 }
