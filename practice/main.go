e73a586f1330def6ad672f56695e358a9ae4c632
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-01-21 15:41:58 +0900

    coding

--- practice/main.go
+++ practice/main.go
@@ -2,35 +2,20 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name string
-	Age int
+type MyInt int
+
+func (mi MyInt) Print() {
+	fmt.Println(mi)
 }
 
 func main() {
-	m := map[int]User{
-		1: {Name:"Takenari", Age: 25},
-		2: {Name:"Taro", Age: 5},
-		3: {Name:"Ti", Age: 25},
-		4: {Name:"nari", Age: 25},
-	}
+	var mi MyInt
 
-	fmt.Println(m)
+	fmt.Println(mi)
+	fmt.Printf("%T\n", mi)
 
-	m2 := map[User]string{
-		{Name:"Takenari", Age: 25}: "Tokyo",
-		{Name:"Ti", Age: 25}: "LA",
-	}
+	// i := 100;
+	// fmt.Println(mi + i)
 
-	fmt.Println(m2)
-
-	m3 := make(map[int]User)
-	fmt.Println(m3)
-	m3[1] = User{Name: "user3"}
-	fmt.Println(m3)
-
-	for _, v := range m3 {
-		fmt.Println(v)
-	}
-
+	mi.Print()
 }
