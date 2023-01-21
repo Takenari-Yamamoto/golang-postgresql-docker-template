d640a117b6bb4a899bd15c71ce3057db006dd499
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-01-21 14:40:40 +0900

    coding

--- practice/main.go
+++ practice/main.go
@@ -2,48 +2,27 @@ package main
 
 import "fmt"
 
+type T struct {
+	User User
+}
+
 type User struct {
 	Name string
 	Age int
 }
 
-func UpdateUser(user User) {
-	user.Name = "A"
-	user.Age = 1000
+func (u User) SayName() {
+	fmt.Println(u.Name)
 }
 
-func UpdateUser2(user *User) {
-	user.Name = "A"
- 	user.Age = 1000
+func (u *User) SetName(name string) {
+	u.Name = name
 }
 
 func main() {
-	var user1 User
-	user1.Name = "Takenari"
-	user1.Age = 25
-	fmt.Println(user1)
+	user1 := User{Name: "Take"}
+	user1.SayName()
 
-	user2 := User{}
-	user2.Name = "Miki"
-	fmt.Println(user2)
-
-	User3 := User{Name: "Shiba", Age: 25}
-	fmt.Println(User3)
-
-	user4 := User{"Yuma", 25}
-	fmt.Println(user4)
-
-	// new を使うと構造体のポインタ型を返す
-	user5 := new(User)
-	fmt.Println(user5)
-
-	// アドレス演算子を使っても構造体のポインタ型を返す
-	user6 := &User{}
-	fmt.Println(user6)
-
-	UpdateUser(user1)
-	UpdateUser2(user6)
-
-	// fmt.Println(user1)
-	fmt.Println(user1)
+	user1.SetName("SAKE")
+	user1.SayName()
 }
