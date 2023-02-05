f5ba049fb56f7d6a83a408cff2325e0529f394d5
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-02-05 13:13:44 +0900

    db疎通

--- 03_golang-postgresql-docker/backend/go.mod
+++ 03_golang-postgresql-docker/backend/go.mod
@@ -1,3 +1,5 @@
 module golang-postgresql-docker
 
 go 1.18
+
+require github.com/lib/pq v1.10.7
--- 03_golang-postgresql-docker/backend/main.go
+++ 03_golang-postgresql-docker/backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-postgresql-docker/database"
+)
 
 func main () {
 	fmt.Println("Hello world")
+	db, err := database.NewDb()
+	if err != nil {
+		fmt.Println("エラーだよ",err)
+		return
+	}
+
+	fmt.Println("connect to success", db)
 }
