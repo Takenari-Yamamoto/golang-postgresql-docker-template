512b5f7621df4e72ba008cc7e210a84943648ce9
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-02-12 09:52:23 +0900

    一旦コミット

--- backend/repository/todo.go
+++ backend/repository/todo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"golang-postgresql-docker/database"
 	"golang-postgresql-docker/database/models"
+	gql "golang-postgresql-docker/graph/model"
 
 	"github.com/google/uuid"
 	"github.com/volatiletech/sqlboiler/v4/boil"
@@ -16,11 +17,42 @@ func NewTodoRepository() *TodoRepo {
 	return &TodoRepo{}
 }
 
-func (repo TodoRepo) Insert(title, content string) {
+func (repo *TodoRepo) FindById(id string) (*models.Todo, error) {
 	db, err := database.NewDb()
 	if err != nil {
 		fmt.Println("エラーだよ",err)
-		return
+		return nil, err
+	}
+	todo, err := models.Todos(
+		models.TodoWhere.ID.EQ(id),
+	).One(context.Background(), db)
+	if err != nil {
+		return nil, err
+	}
+
+	return todo, nil
+}
+
+
+func (repo *TodoRepo) GetAll() (models.TodoSlice, error) {
+db, err := database.NewDb()
+	if err != nil {
+		fmt.Println("エラーだよ",err)
+		return nil, err
+	}
+	todos, err := models.Todos().All(context.Background(), db)
+	if err != nil {
+		return nil, err
+	}
+
+	return todos, nil
+}
+
+func (repo *TodoRepo) Insert(title, content string) (*models.Todo, error) {
+	db, err := database.NewDb()
+	if err != nil {
+		fmt.Println("エラーだよ",err)
+		return nil, err
 	}
 
 	uuidObj, _ := uuid.NewUUID()
@@ -29,6 +61,13 @@ func (repo TodoRepo) Insert(title, content string) {
 		Title: title,
 		Content: content,
 	}
-	todo.Insert(context.Background(), db, boil.Infer())
+	if err := todo.Insert(context.Background(), db, boil.Infer()); err != nil {
+		return nil, err
+	}
+
+	return todo, nil
+}
+
+func convModelToGql(*models.Todo) *gql.Todo {
 
 }
